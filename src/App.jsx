b4c49379b582de6c798a7a0a1010a86aadfc746d
kaceybolman <kaceyfaltusibed@yahoo.com>
2018-10-30 01:48:41 +0800

    Add ChartContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
+import { TVChartContainer } from './components/TVChartContainer/index';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className={ 'App' }>
 				Example for Trading View Chart
+				<TVChartContainer />
 			</div>
 		);
 	}
 }
 
 export default App;
+
